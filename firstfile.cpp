d3bbba3fcfa3dc5f8faabcb29cafd8688998f1f1
tarun-reddy-13 <tarunreddythota13@gmail.com>
2024-04-28 22:21:20 +0530

    get, ignore

--- firstfile.cpp
+++ firstfile.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
+
 using namespace std;
+
 int main(){
+
+    char first;
     cout<<"Namasthe Dunia"<<endl;
+    first = cin.get();
+    cin.ignore(2,' '); //ignores 2 charecters if space is encountered.
+    cout<<first;
+    cin>>first;
+    cout<<first;
     return 0;
+
 }
--- inputDemo.cpp
+++ inputDemo.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+    //Declaring a variable 'a' of type 'int'
     int a;
+    //taking input for the variable 'a' using cin
     cin>>a;
+    //printing the content of variable 'a' using cout
     cout<<a;
     return 0;
 }
